1486b20eb61451e25ad2b21ecb97fddae4972720
Angeall <angeal1105@gmail.com>
2015-10-07 17:40:59 +0200

    first version of nao_controller with camera bindings

--- cameraTest.py
+++ cameraTest.py
@@ -28,25 +28,48 @@ def test():
     switch = '0 : OFF \n1 : ON'
     cv2.createTrackbar(switch, 'Control',0,1,nothing)
     while True:
-        hasRead, frame = cap.read()
+        hasRead, img = cap.read()
         if not hasRead:
             print "Image not readable"
             break
-        newImg = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        thresholdedImg = cv2.inRange(newImg, np.array([cv2.getTrackbarPos("LowH", "Control"),
-                                                       cv2.getTrackbarPos("LowS", "Control"),
-                                                       cv2.getTrackbarPos("LowV", "Control")]),
-                                     np.array([cv2.getTrackbarPos("HighH", "Control"),
-                                               cv2.getTrackbarPos("HighS", "Control"),
-                                               cv2.getTrackbarPos("HighV", "Control")]))
-        cv2.erode(thresholdedImg, thresholdedImg, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5,5)))
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        gray = cv2.GaussianBlur(gray, (5, 5), 0)
+        thresholdedImg = cv2.Canny(gray, 35, 50, L2gradient=True)
+        _, contours, h = cv2.findContours(thresholdedImg, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_TC89_KCOS)
+        for cnt in contours:
+            area = cv2.contourArea(cnt)
+            if area > 500:
+                approx = cv2.approxPolyDP(cnt,0.01*cv2.arcLength(cnt,True),True)
+                print len(approx)
+                if len(approx)==5:
+                    print "pentagon"
+                    cv2.drawContours(img,[cnt],0,255,-1)
+                elif len(approx) > 15:
+                    print "circle"
+                    cv2.drawContours(img,[cnt],0,(0,255,255),-1)
+                elif len(approx)==3:
+                    print "triangle"
+                    cv2.drawContours(img,[cnt],0,(0,255,0),-1)
+                elif len(approx)==4:
+                    print "square"
+                    cv2.drawContours(img,[cnt],0,(0,0,255),-1)
+                elif len(approx) == 9:
+                    print "half-circle"
+                    cv2.drawContours(img,[cnt],0,(255,255,0),-1)
+        # newImg = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        # thresholdedImg = cv2.inRange(newImg, np.array([cv2.getTrackbarPos("LowH", "Control"),
+        #                                                cv2.getTrackbarPos("LowS", "Control"),
+        #                                                cv2.getTrackbarPos("LowV", "Control")]),
+        #                              np.array([cv2.getTrackbarPos("HighH", "Control"),
+        #                                        cv2.getTrackbarPos("HighS", "Control"),
+        #                                        cv2.getTrackbarPos("HighV", "Control")]))
+        # cv2.erode(thresholdedImg, thresholdedImg, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5,5)))
         # cv2.dilate(thresholdedImg, thresholdedImg, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5,5)))
         #
         # cv2.dilate(thresholdedImg, thresholdedImg, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5,5)))
         # cv2.erode(thresholdedImg, thresholdedImg, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5,5)))
-
         cv2.imshow("Thresholded Image", thresholdedImg)
-        cv2.imshow("Original Image", frame)
+        cv2.imshow("Original Image", img)
 
         if cv2.waitKey(1) == 27:
             print "Esc pressed : exit"
---
+++ nao/nao_controller.py
@@ -0,0 +1,37 @@
+__author__ = 'Angeall'
+from naoqi import ALProxy
+import numpy as np
+
+class NAOController:
+    def __init__(self, robot_ip, port):
+        self.motion_proxy = ALProxy("ALMotion", robot_ip, port)
+        self.video_device = ALProxy("ALVideoDevice", robot_ip, port)
+        self.subscriber_id = None
+        self.motion_proxy.wakeUp()
+        self.motion_proxy.setCollisionProtectionEnabled("Arms", True)
+
+    def connect_to_camera(self, subscriber_id="Connect4NAO", camera_num=0, res=2, color_space=0, fps=30):
+        try:
+            self.subscriber_id = self.video_device.subscribeCamera(subscriber_id, camera_num, res, color_space, fps)
+        except BaseException, err:
+            print "ERR: cannot connect to camera : %s" % err
+            return -1
+        return 0
+
+    def disconnect_from_camera(self):
+        try:
+            self.video_device.unsubscribe(self.subscriber_id)
+        except BaseException, err:
+            print "ERR: cannot disconnect from camera : %s" % err
+
+    def get_image_from_camera(self):
+        try:
+            img = self.video_device.getImageRemote(self.subscriber_id)
+            if img is not None:
+                converted_img = np.reshape(np.frombuffer(img[6], dtype='%iuint8' % img[2]),(img[1], img[0], img[2]))
+                return converted_img
+            else:
+                return None
+        except BaseException, err:
+            print "ERR: cannot get image from camera : %s" % err
+            return None
