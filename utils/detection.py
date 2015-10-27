fe7c82c363a920325caacc106a1d8d20e469401d
Angeall <angeal1105@gmail.com>
2015-10-27 17:55:30 +0100

    refactor returned values for graph connections in order to do a clustering

--- utils/detection.py
+++ utils/detection.py
@@ -1,11 +1,10 @@
 __author__ = 'Angeall'
-
 import numpy as np
 
 
 # Returns the vector made by the two input points
 def vectorize(p1, p2):
-    return abs(int(p2[0]) - int(p1[0])), abs(int(p2[1]) - int(p1[1]))
+    return [int(p2[0]) - int(p1[0]), int(p2[1]) - int(p1[1])]
 
 
 # Returns the distance between two points (the norm of the vector made by the input two points)
@@ -19,9 +18,9 @@ def point_distance(p1, p2):
 #           if there's no keypoint c such that ((dist(a,c) < dist(a,b)) and (dist(c, b) < dist(a, c)))
 #           then add the vector (a,b) to the connection with the indices of the keypoints a and b
 # Elements of this list are :
-#   (vector, (circle_list_index_1, circle_list_index_1))
+#   [vectors, circle_list_indices]
 def connect_keypoints(circles):
-    connections = []
+    connections = [[],[]]
     for i, keypoint in enumerate(circles):
         for j, compare_point in enumerate(circles):
             couple_dist = point_distance((keypoint[0], keypoint[1]), (compare_point[0], compare_point[1]))
@@ -34,29 +33,34 @@ def connect_keypoints(circles):
                         if k_c_dist < couple_dist and comp_c_dist < couple_dist:
                             closer = False
                 if closer:
-                    connections.append((vectorize((circles[i][0], circles[i][1]), (circles[j][0], circles[j][1])),
-                                        (i, j)))
+                    connections[0].append(vectorize((circles[i][0], circles[i][1]), (circles[j][0], circles[j][1])))
+                    connections[1].append([i, j])
     return connections
 
 
 def filter_connections(connections, pixel_threshold=20, min_to_keep=5):
-    to_keep = []
-    for connection in connections:
+    to_keep = [[], []]
+    for i, connection in enumerate(connections[0]):
         similar_counter = 0
-        for other in connections:
+        for other in connections[0]:
             if not(connection is other):
-                (x_diff, y_diff) = vectorize(connection[0], other[0])
+                (x_diff, y_diff) = vectorize(connection, other)
                 if x_diff < pixel_threshold and y_diff < pixel_threshold:
                     similar_counter += 1
         if similar_counter > min_to_keep:
-            to_keep.append(connection)
+            to_keep[0].append(connection)
+            to_keep[1].append(connections[1][i])
     return to_keep
 
 
+def cluster_vectors(filtered_connections, nb_clusters=4,):
+    print "TODO"
+
+# TODO : Install scipy + scipy.cluster.vq.kmeans
 # TODO : unit_test filter_connection with dumb values
 
-"""
+
 circle_list= [(0,  10), (6, 10), (0, 7), (6, 7)]
 print connect_keypoints(circle_list)
-"""
+
 
