40eac237ab53ecd264938961ffa678ea734db96f
Angeall <angeal1105@gmail.com>
2016-04-07 13:19:08 +0200

    Readme fixed

--- src/connect4/detector/upper_hole.py
+++ src/connect4/detector/upper_hole.py
@@ -1,4 +1,5 @@
 __author__ = 'Anthony Rouneau'
+import cv2
 
 
 class UpperHoleDetector(object):
@@ -6,5 +7,24 @@ class UpperHoleDetector(object):
 
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, rectangles, lines):
+        self.rectangles = rectangles
+        self.lines = lines
+
+
+    @staticmethod
+    def rectangles_included(rect1, rect2):
+        """
+        Checks if the first rectangle is included in the second rectangle
+        :param rect1: the first rectangle
+        :param rect2: the second rectangle
+        :return: true if the first rectangle is included in the second one
+        :rtype: bool
+        """
+        # The order in the result of the boxPoints : bottom_l, up_l, up_r, bottom_r.
+        ((left_x1, down_y1), (_, up_y1), (right_x1, _), (_, _)) = cv2.boxPoints(rect1)
+        ((left_x2, down_y2), (_, up_y2), (right_x2, _), (_, _)) = cv2.boxPoints(rect2)
+        if()
+
+
+
